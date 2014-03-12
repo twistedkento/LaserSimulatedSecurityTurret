eadee59d45b478d6d802f83e6bcc2640253439d2
davidkron <david_jensen3@hotmail.com>
2014-03-12 10:54:28 +0100

    Added basic turn test

--- Source/test.py
+++ Source/test.py
@@ -1,20 +1,13 @@
 import unittest
+import turret
 
-# Here's our "unit".
-def IsOdd(n):
-    return n % 2 == 1
-
-# Here's our "unit tests".
 class IsOddTests(unittest.TestCase):
 
-    def testOne(self):
-        self.assertTrue(IsOdd(1))
-
-    def testTwo(self):
-        self.assertFalse(IsOdd(2))
+	def testOne(self):
+		turret.turnRight()
 
 def main():
-    unittest.main()
+	unittest.main()
 
 if __name__ == '__main__':
-    main()
+	main()
---
+++ Source/turret.py
@@ -0,0 +1,9 @@
+angle = 0
+
+def turnLeft():
+	global angle
+	angle -= 1
+	
+def turnRight():
+	global angle
+	angle -= 1
