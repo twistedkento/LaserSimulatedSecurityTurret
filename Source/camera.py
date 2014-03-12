c6601f6b4afee906cfdfecab00a4d9df664b6db5
tommy-m <tommymolin161@hotmail.com>
2014-03-12 12:09:29 +0100

    camera now only checks if there's a visible target, which is always true for now.

--- Source/camera.py
+++ Source/camera.py
@@ -1,15 +1,2 @@
-scanning = False
-
-def start_scanning():
-    global scanning
-    scanning = True
-    print("Camera starts scanning")
-
-def stop_scanning():
-    global scanning
-    scanning = False
-    print("Camera stops scanning")
-
-def get_state():
-    global scanning
-    return scanning
+def visible_target():
+    return True
