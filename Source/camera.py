d1b178ad54109de4014f51374af6a256f4f99ff6
tommy-m <tommymolin161@hotmail.com>
2014-03-12 11:47:59 +0100

    added print lines to the camera to get some feedback when testing

--- Source/camera.py
+++ Source/camera.py
@@ -3,10 +3,12 @@ scanning = False
 def start_scanning():
     global scanning
     scanning = True
+    print("Camera starts scanning")
 
 def stop_scanning():
     global scanning
     scanning = False
+    print("Camera stops scanning")
 
 def get_state():
     global scanning
