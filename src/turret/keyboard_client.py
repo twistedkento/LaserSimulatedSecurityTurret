66e2a37e49c529bf7e7db2adcc64b144b3f6976f
twistedretard <kenny@kento.se>
2014-04-28 02:33:26 +0200

    Added control over tcp and a keyboard controller

---
+++ src/turret/keyboard_client.py
@@ -0,0 +1,77 @@
+import socket
+import time
+import curses
+from turret_command import TurretCommand as command
+
+servo = command.ServoState
+laser = command.LaserState
+
+if __name__ == '__main__':
+    #Init curses
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(1)
+    stdscr.leaveok(1)
+
+    laserstate = True
+
+    HOST, PORT = "localhost", 9999
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        sock.connect((HOST, PORT))
+        stdscr.nodelay(1)
+        while True:
+            #Handle key input
+            data = command()
+            if laserstate:
+                data.laser = laser.on
+            else:
+                data.laser = laser.off
+            c = stdscr.getch()
+            if not c == -1:
+                if c == 27:
+                    break
+                if c == curses.KEY_LEFT:
+                    data.servo_x = servo.inc
+                    curses.flushinp()
+                elif c == curses.KEY_RIGHT:
+                    data.servo_x = servo.dec
+                    curses.flushinp()
+                if c == curses.KEY_UP:
+                    data.servo_y = servo.inc
+                elif c == curses.KEY_DOWN:
+                    data.servo_y = servo.dec
+                    curses.flushinp()
+                if c == ord(' '):
+                    laserstate = not laserstate
+                    if laserstate:
+                        data.laser = laser.on
+                    else:
+                        data.laser = laser.off
+                    curses.flushinp()
+            else:
+                curses.flushinp()
+                time.sleep(0.1)
+                continue
+            #Send keys
+            stdscr.addstr(0,0,'Sending: '+ str(data))
+            stdscr.refresh()
+            sock.send(bytes(data))
+            status = sock.recv(1)
+            time.sleep(0.04)
+            try:
+                if not int.from_bytes(status, 'little'):
+                    break
+            except:
+                break
+    except ConnectionRefusedError:
+        print("Error connecting to server")
+    finally:
+        sock.close()
+        #Reset curses
+        curses.nocbreak()
+        stdscr.keypad(0)
+        curses.echo()
+        curses.endwin()
---
+++ src/turret/turret_tcphandler.py
@@ -0,0 +1,24 @@
+import socketserver
+from turret_command import TurretCommand as command
+
+class TurretTCPHandler(socketserver.BaseRequestHandler):
+
+    def setup(self):
+        self.server.connected_clients += 1
+
+    def handle(self):
+        while True:
+            try:
+                data = self.request.recv(1024)
+                if not data:
+                    break
+                self.server.command_queue.put(command(data))
+            except:
+                break
+
+            #Sending ok signal!
+            #This avoids getting data accumulation on network lag.
+            self.request.send((1).to_bytes(1, 'little'))
+
+    def finish(self):
+        self.server.connected_clients -= 1
