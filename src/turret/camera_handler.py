1a7200bbc0e1f722c1348d6831f525b31beb4b70
twistedretard <kenny@kento.se>
2014-05-20 22:03:56 +0200

    HERP DERP MUERP! Forgot the camera code...

---
+++ src/turret/camera_handler.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+from subprocess import Popen,PIPE
+
+
+class CameraHandler(object):
+    '''
+        Class for the cameralogic servervise
+    '''
+    def __init__(self):
+        super(CameraHandler, self).__init__()
+        self.__video_command = None
+        self.__send_command = None
+        self.__connected_client = None
+
+    def send_camera(self, caddr):
+        if self.__connected_client == caddr:
+            self.__video_command.terminate()
+            self.__send_command.terminate()
+            self.__connected_client = None
+        else:
+            self.__video_command = Popen(['/opt/vc/bin/raspivid', '-u', '-n','-w','800','-h','-fps','24','-t','0','-o','-'], stdout=PIPE, close_fds=False)
+            self.__send_command = Popen(['socat', '-', 'udp-sendto:' + caddr[0] + ':' + str(caddr[1]) ], stdin=self.__video_command.stdout,close_fds=False)
+            self.__connected_client = caddr
